b9f8e62658589e2765969be88b8ae340f3fcb2d2
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-29 15:36:00 -0300

    feat: add TransactionBehavior and ValidationBehavior for request processing

---
+++ src/PortfolioScheduler.Application/Behaviors/TransactionBehavior.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System.Transactions;
+
+namespace PortfolioScheduler.Application.Behaviors;
+
+public class TransactionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+        var response = await next();
+
+        scope.Complete();
+
+        return response;
+    }
+}
