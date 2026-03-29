a5370fa0c6e5927df210d231efa727528d499536
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-29 15:34:41 -0300

    feat: add DependencyInjection class for application and infrastructure setup

---
+++ src/PortfolioScheduler.Infra/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PortfolioScheduler.Domain.Repositories;
+using PortfolioScheduler.Infra.Data.Context;
+using PortfolioScheduler.Infra.Persistence;
+
+namespace PortfolioScheduler;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // DbContext
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("PortfolioScheduler-Connection")));
+
+        // Repositories
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+        return services;
+    }
+}
+
+
