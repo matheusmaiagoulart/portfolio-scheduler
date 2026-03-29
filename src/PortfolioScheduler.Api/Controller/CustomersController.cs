26b75c2cb2970cffc7c6d27062fa9503c198b185
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-29 15:36:36 -0300

    feat: add CustomersController for handling customer registration requests

---
+++ src/PortfolioScheduler.Api/Controller/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioScheduler.Api.Extensions;
+using PortfolioScheduler.Application.Commands.RegisterCustomerSubscriber;
+
+namespace PortfolioScheduler.Api.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public CustomersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterCustomerSubscriber(RegisterCustomerSubscriberCommand request)
+    {
+        var result = await _mediator.Send(request);
+        return result.ValidateResult(this);
+    }
+}
