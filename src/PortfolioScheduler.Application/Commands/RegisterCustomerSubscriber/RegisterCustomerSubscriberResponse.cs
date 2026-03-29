439e2ce8809e834254d8a7d4f3cc47c607898c66
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-29 15:35:26 -0300

    feat: implement RegisterCustomerSubscriber Command, with command entry, handler, response, and validator

---
+++ src/PortfolioScheduler.Application/Commands/RegisterCustomerSubscriber/RegisterCustomerSubscriberResponse.cs
@@ -0,0 +1,27 @@
+﻿
+using PortfolioScheduler.Domain.Entities;
+
+namespace PortfolioScheduler.Application.Commands.RegisterCustomerSubscriber;
+
+public class RegisterCustomerSubscriberResponse
+{
+    public long Id { get; init; }
+    public string Name { get; init; }
+    public string Message { get; init; }
+    public decimal MonthlyAmount { get; init; }
+
+    private RegisterCustomerSubscriberResponse(long id, string name, decimal monthlyAmount)
+    {
+        Id = id;
+        Name = name;
+        MonthlyAmount = monthlyAmount;
+    }
+
+    public static RegisterCustomerSubscriberResponse SuccessRegister(Customer customer)
+    {
+        return new RegisterCustomerSubscriberResponse(customer.Id, customer.Name, customer.MonthlyAmount)
+        {
+            Message = $"Congrats, {customer.Name}! Your subscription has been successfully registered."
+        };
+    }
+}
