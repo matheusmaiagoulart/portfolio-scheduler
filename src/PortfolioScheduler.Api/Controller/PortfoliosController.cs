5be0a55c089645b7cca9d01a3e05778d8aef2e1a
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-31 10:59:26 -0300

    feat: add PortfoliosController for handling portfolio creation requests

---
+++ src/PortfolioScheduler.Api/Controller/PortfoliosController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioScheduler.Api.Extensions;
+using PortfolioScheduler.Application.Commands.CreateRecommendedPortfolio;
+
+namespace PortfolioScheduler.Api.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PortfoliosController : ControllerBase
+{
+
+    private readonly IMediator _mediator;
+    public PortfoliosController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateRecommendedPortfolio(CreateRecommendedPortfolioCommand request)
+    {
+        var result = await _mediator.Send(request);
+        return result.ValidateResult(this);
+    }
+}
