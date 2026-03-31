94b81a440f98cb18fdc4d0f47a9f1a0eb22d4cba
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-31 15:31:51 -0300

    feat: update ResultMapping to support customizable success status codes

--- src/PortfolioScheduler.Api/Extensions/ResultMapping.cs
+++ src/PortfolioScheduler.Api/Extensions/ResultMapping.cs
@@ -5,10 +5,10 @@ namespace PortfolioScheduler.Api.Extensions;
 
 public static class ResultMapping
 {
-    public static IActionResult ValidateResult<T>(this Result<T> result, ControllerBase controller)
+    public static IActionResult ValidateResult<T>(this Result<T> result, ControllerBase controller, int successStatusCode)
     {
         if (result.IsSuccess)
-            return controller.Ok(result.Value);
+            return controller.StatusCode(successStatusCode, result.Value);
 
         var error = result.Errors.First();
         var statusCode = error.Metadata.TryGetValue("statusCode", out var code) ? (int)code : 500;
@@ -16,10 +16,10 @@ public static class ResultMapping
         return controller.StatusCode(statusCode, new { error = error.Message });
     }
 
-    public static IActionResult ToActionResult(this Result result, ControllerBase controller)
+    public static IActionResult ToActionResult(this Result result, ControllerBase controller, int successStatusCode = 200)
     {
         if (result.IsSuccess)
-            return controller.Ok();
+            return controller.StatusCode(successStatusCode);
 
         var error = result.Errors.First();
         var statusCode = error.Metadata.TryGetValue("statusCode", out var code) ? (int)code : 500;
