17aa35ca8da0b7e8b4bf163b5f022047a1a979ee
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-31 10:57:42 -0300

    feat: implement IRecommendedPortfolioRepository and RecommendedPortfolioRepository for portfolio management

---
+++ src/PortfolioScheduler.Domain/Repositories/IRecommendedPortfolioRepository.cs
@@ -0,0 +1,12 @@
+﻿using FluentResults;
+using PortfolioScheduler.Domain.Entities;
+
+namespace PortfolioScheduler.Domain.Repositories;
+
+public interface IRecommendedPortfolioRepository
+{
+    Task AddAsync(RecommendedPortfolio recommendedPortfolio, CancellationToken ct);
+    Task<RecommendedPortfolio> GetByIdAsync(long id, CancellationToken ct);
+    void Update(RecommendedPortfolio recommendedPortfolio);
+    Task<Result> SaveChangesAsync(long portfolioId, CancellationToken ct);
+}
