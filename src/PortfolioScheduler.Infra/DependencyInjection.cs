1b7eadd4c169643cb1536685d37dfbc39582bf11
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-21 11:22:41 -0300

    feat: register new repositories and services in dependency injection

--- src/PortfolioScheduler.Infra/DependencyInjection.cs
+++ src/PortfolioScheduler.Infra/DependencyInjection.cs
@@ -2,7 +2,10 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PortfolioScheduler.Domain.Repositories;
+using PortfolioScheduler.Domain.Services.Implementations;
+using PortfolioScheduler.Domain.Services.Interfaces;
 using PortfolioScheduler.Infra.Data.Context;
+using PortfolioScheduler.Infra.ExternalData.B3;
 using PortfolioScheduler.Infra.Persistence;
 
 namespace PortfolioScheduler;
@@ -19,7 +22,16 @@ public static class DependencyInjection
 
         // Repositories
         services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IAssetPricesRepository, AssetPricesRepository>();
+        services.AddScoped<IPurchaseOrdersRepository, PurchaseOrdersRepository>();
         services.AddScoped<IRecommendedPortfolioRepository, RecommendedPortfolioRepository>();
+
+        services.AddScoped<ICotahistParser, CotahistParser>();
+
+        // Domain Services
+        services.AddScoped<IPurchaseExecutionDateValidator, PurchaseExecutionDateValidator>();
+        services.AddScoped<IPurchaseQuotesCalculator, PurchaseQuotesCalculator>();
+        services.AddScoped<IPortfolioDistribuition, PortfolioDistribuition>();
 
         return services;
     }
