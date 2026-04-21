831d9b139bfcbeacb2d06f363be63d197925c9b4
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-21 11:17:01 -0300

    feat: add PurchaseRoundDataDTO and CustodyPurchaseDataDTO for managing purchase round data

---
+++ src/PortfolioScheduler.Domain/Services/DTOs/PurchaseRoundDataDTO.cs
@@ -0,0 +1,16 @@
+﻿using PortfolioScheduler.Domain.Entities;
+
+namespace PortfolioScheduler.Domain.Services.DTOs;
+
+public record PurchaseRoundDataDTO(
+    decimal TotalPurchaseAmount,
+    Dictionary<long, CustodyPurchaseDataDTO> CustodiesPerCustomer);
+
+public record CustodyPurchaseDataDTO
+{
+    public long CustomerId { get; init; }
+    public string FullName { get; init; }
+    public long BrokerageAccountId { get; init; }
+    public decimal ThirdPartyBalance { get; init; }
+    public IEnumerable<Custody> CustomerCustodies { get; init; }
+}
