dc9cdd794634a789e306c0dc31fe64d92658023e
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-21 11:17:22 -0300

    feat: add QuoteDTO for managing stock quote data

---
+++ src/PortfolioScheduler.Domain/Services/DTOs/QuoteDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace PortfolioScheduler.Domain.Services.DTOs;
+
+public class QuoteDTO
+{
+    public DateTime DataPregao { get; set; }
+    public string Ticker { get; set; } = string.Empty;
+    public string BDICode { get; set; } = string.Empty;
+    public int MarketType { get; set; }
+    public string CompanyName { get; set; } = string.Empty;
+    public decimal OpenPrice { get; set; }
+    public decimal MaxPrice { get; set; }
+    public decimal MinPrice { get; set; }
+    public decimal ClosePrice { get; set; }
+    public decimal AveragePrice { get; set; }
+    public long TradeQuantity { get; set; }
+    public decimal TradedVolume { get; set; }
+}
