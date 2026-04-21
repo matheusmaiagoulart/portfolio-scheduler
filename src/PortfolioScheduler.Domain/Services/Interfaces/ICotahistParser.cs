06ee61c09182f6e0ef42cf2383be4199089dfc55
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-21 11:20:09 -0300

    feat: add ICotahistParser interface for parsing Cotahist data and validating its existence

---
+++ src/PortfolioScheduler.Domain/Services/Interfaces/ICotahistParser.cs
@@ -0,0 +1,10 @@
+﻿using FluentResults;
+using PortfolioScheduler.Domain.Services.DTOs;
+
+namespace PortfolioScheduler.Domain.Services.Interfaces;
+
+public interface ICotahistParser
+{
+    IEnumerable<QuoteDTO> ParseArquivo(HashSet<string> tickers, string caminhoArquivo);
+    Result<string> ExistsCotahist(DateOnly referenceDate);
+}
