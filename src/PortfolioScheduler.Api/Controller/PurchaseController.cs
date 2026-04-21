0863ce2c0976aa1c35272527a09e3ea808e6436f
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-21 10:54:47 -0300

    feat: add PurchaseController for executing portfolio purchases and importing stock quotes

---
+++ src/PortfolioScheduler.Api/Controller/PurchaseController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioScheduler.Api.Extensions;
+using PortfolioScheduler.Application.Commands.ExecutePortfolioPurchase;
+using PortfolioScheduler.Application.Commands.ImportStockQuotes;
+
+namespace PortfolioScheduler.Api.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PurchaseController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public PurchaseController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("/execute-purchase")]
+    public async Task<IActionResult> ExecutePurchase(ImportStockQuotesCommand request)
+    {
+        var result = await _mediator.Send(request);
+        if (!result.IsSuccess)
+            return result.ValidateResult(this, 200);
+
+        var purchaseResult = await _mediator.Send(new ExecutePortfolioPurchaseCommand(result.Value));
+        if (!purchaseResult.IsSuccess)
+            return purchaseResult.ValidateResult(this, 200);
+
+        return purchaseResult.ValidateResult(this, 201);
+    }
+}
