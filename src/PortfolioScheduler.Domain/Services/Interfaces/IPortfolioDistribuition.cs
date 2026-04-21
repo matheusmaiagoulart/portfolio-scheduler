2639e52c8233b2d690fbe3e147e03a49fd0d6f84
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-21 11:20:24 -0300

    feat: add IPortfolioDistribution interface for managing asset distribution logic

---
+++ src/PortfolioScheduler.Domain/Services/Interfaces/IPortfolioDistribuition.cs
@@ -0,0 +1,10 @@
+﻿using FluentResults;
+using PortfolioScheduler.Domain.Entities;
+using PortfolioScheduler.Domain.Services.DTOs;
+
+namespace PortfolioScheduler.Domain.Services.Interfaces;
+
+public interface IPortfolioDistribuition
+{
+    Result<DistributionResultDTO> Distribute(IEnumerable<PurchaseOrder> purchasedAssets, PurchaseRoundDataDTO purchaseRoundData, List<PortfolioItem> portfolio, BrokerageAccount masterAccount);
+}
