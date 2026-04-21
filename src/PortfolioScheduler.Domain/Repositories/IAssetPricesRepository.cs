dcad3034f8e0b017ed064ec22ae82d1e768bd274
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-21 11:15:37 -0300

    feat: add repositories for asset prices and purchase orders with async methods

---
+++ src/PortfolioScheduler.Domain/Repositories/IAssetPricesRepository.cs
@@ -0,0 +1,11 @@
+﻿using FluentResults;
+using PortfolioScheduler.Domain.Entities;
+
+namespace PortfolioScheduler.Domain.Repositories;
+
+public interface IAssetPricesRepository
+{
+    Task<IEnumerable<AssetPrices>> GetAssetPricesByDateAsync(DateTime tradingDate, CancellationToken ct);
+    Task AddAsync (IEnumerable<AssetPrices> assetPrices);
+    Task<Result> SaveChangesAsync(IEnumerable<AssetPrices> assetPrices, CancellationToken ct);
+}
