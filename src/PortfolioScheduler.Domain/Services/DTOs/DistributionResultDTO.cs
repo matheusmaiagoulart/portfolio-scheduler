ae6f62ce8352557f533feea51fec6a8e7b484ca6
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-21 11:16:29 -0300

    feat: add DistributionResultDTO for managing distribution results and purchase orders

---
+++ src/PortfolioScheduler.Domain/Services/DTOs/DistributionResultDTO.cs
@@ -0,0 +1,37 @@
+﻿using static PortfolioScheduler.Domain.Services.DTOs.DistributionResultDTO;
+
+namespace PortfolioScheduler.Domain.Services.DTOs;
+
+public record DistributionResultDTO(
+    IReadOnlyCollection<PurchaseOrdersPerAsset> purchaseOrders,
+    IReadOnlyList<Distribuitions> distribuitions,
+    List<ResidualsFromMaster> ResidualsFromMasterAccount)
+{
+    public record Distribuitions(
+        long CustomerId,
+        string FullName,
+        decimal ThirdPartyBalance,
+        IEnumerable<DistribuitionPerAsset> DistribuitionsPerAsset
+    );
+    public record DistribuitionPerAsset(
+       string Ticker,
+       int Quantity
+    );
+
+    public record PurchaseOrdersPerAsset(
+    string Ticker,
+    int TotalQuantity,
+    PurchaseOrdersPerAssetDetails Details,
+    decimal UnitPrice)
+    {
+        public decimal TotalPrice => UnitPrice * TotalQuantity;
+    };
+
+    public record PurchaseOrdersPerAssetDetails(
+        string Type,
+        string Ticker,
+        int Quantity
+    );
+
+    public record ResidualsFromMaster(string Ticker, int Quantity);
+}
