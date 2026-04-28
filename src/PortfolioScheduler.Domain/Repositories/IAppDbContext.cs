d0e4add7256b2e178a91416cc2386a6238c0850b
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-28 13:52:14 -0300

    feat: add IAppDbContext interface for transaction management

---
+++ src/PortfolioScheduler.Domain/Repositories/IAppDbContext.cs
@@ -0,0 +1,8 @@
+﻿namespace PortfolioScheduler.Domain.Repositories;
+
+public interface IAppDbContext
+{
+    Task BeginTransactionAsync(CancellationToken ct = default);
+    Task CommitAsync(CancellationToken ct = default);
+    Task RollbackAsync(CancellationToken ct = default);
+}
