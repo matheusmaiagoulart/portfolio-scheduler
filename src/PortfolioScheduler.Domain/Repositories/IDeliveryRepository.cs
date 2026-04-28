bfe46cfe49ef8cc432f3a5ef45998ac25ad331a2
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-28 13:52:28 -0300

    feat: add IDeliveryRepository interface for managing deliveries

---
+++ src/PortfolioScheduler.Domain/Repositories/IDeliveryRepository.cs
@@ -0,0 +1,10 @@
+﻿using FluentResults;
+using PortfolioScheduler.Domain.Entities;
+
+namespace PortfolioScheduler.Domain.Repositories;
+
+public interface IDeliveryRepository
+{
+    Task AddAsync(ICollection<Delivery> deliveries, CancellationToken ct);
+    Task<Result> SaveAsync(CancellationToken ct);
+}
