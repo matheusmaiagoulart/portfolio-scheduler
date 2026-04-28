87e64a393702d8f9cfe8bedfd70bd3ca70d70875
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-04-28 13:44:46 -0300

    feat: update ValidationBehavior to allow nullable validator and skip validation for handlers without a body

--- src/PortfolioScheduler.Application/Behaviors/ValidationBehavior.cs
+++ src/PortfolioScheduler.Application/Behaviors/ValidationBehavior.cs
@@ -5,19 +5,19 @@ namespace PortfolioScheduler.Application.Behaviors;
 
 public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
 {
-    private readonly IValidator<TRequest> _validator;
+    private readonly IValidator<TRequest>? _validator;
 
-    public ValidationBehavior(IValidator<TRequest> validator)
+    public ValidationBehavior(IValidator<TRequest>? validator)
     {
         _validator = validator;
     }
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        var result = await _validator.ValidateAsync(request, cancellationToken);
-
-        if(_validator == null)
+        if (_validator == null) // For handlers that don't have a body, we just skip validation
             return await next();
+
+        var result = await _validator.ValidateAsync(request, cancellationToken);
 
         if (!result.IsValid)
             throw new ValidationException(result.Errors);
