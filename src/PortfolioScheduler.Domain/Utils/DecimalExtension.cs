d7013d1c9a4e77d8e722ee9582d837fb69aabef1
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-05-01 10:17:03 -0300

    feat: add DecimalExtension class for money and percentage formatting

---
+++ src/PortfolioScheduler.Domain/Utils/DecimalExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace PortfolioScheduler.Domain.Utils;
+
+public static class DecimalExtension
+{
+    public static decimal ToMoney(this decimal value)
+    {
+        return Math.Round(value, 2);
+    }
+
+    public static decimal ToPercentage(this decimal value)
+    {
+        return Math.Round(value, 2);
+    }
+}
