33da585d018b9e8d4aa0826ed4ffe1f5ce5ac68d
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-05-01 10:15:55 -0300

    feat: register CustomerReadRepository for query operations

--- src/PortfolioScheduler.Infra/DependencyInjection.cs
+++ src/PortfolioScheduler.Infra/DependencyInjection.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PortfolioScheduler.Application.Queries.Contracts;
 using PortfolioScheduler.Domain.Repositories;
 using PortfolioScheduler.Domain.Services.Implementations;
 using PortfolioScheduler.Domain.Services.Interfaces;
 using PortfolioScheduler.Infra.Data.Context;
 using PortfolioScheduler.Infra.ExternalData.B3;
 using PortfolioScheduler.Infra.Persistence;
+using PortfolioScheduler.Infra.Queries;
 
 namespace PortfolioScheduler;
 
@@ -20,13 +22,16 @@ public static class DependencyInjection
         services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("PortfolioScheduler-Connection")));
 
-        // Repositories
+        // Repositories Persistence
         services.AddScoped<IAppDbContext, AppDbContext>();
         services.AddScoped<ICustomerRepository, CustomerRepository>();
         services.AddScoped<IDeliveryRepository, DeliveryRepository>();
         services.AddScoped<IAssetPricesRepository, AssetPricesRepository>();
         services.AddScoped<IPurchaseOrdersRepository, PurchaseOrdersRepository>();
         services.AddScoped<IRecommendedPortfolioRepository, RecommendedPortfolioRepository>();
+
+        // Repositories Queries
+        services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
 
         services.AddScoped<ICotahistParser, CotahistParser>();
 
