a615cdabdda461ed3c645ef2adc25da224440f73
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-05-02 20:48:49 -0300

    feat: add GetCurrentRecommendedPortfolio endpoint to PortfoliosController

--- src/PortfolioScheduler.Api/Controller/PortfoliosController.cs
+++ src/PortfolioScheduler.Api/Controller/PortfoliosController.cs
@@ -2,11 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using PortfolioScheduler.Api.Extensions;
 using PortfolioScheduler.Application.Commands.CreateRecommendedPortfolio;
+using PortfolioScheduler.Application.Queries.GetCurrentRecommendedPortfolio;
 
 namespace PortfolioScheduler.Api.Controller;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/admin/cesta")]
 public class PortfoliosController : ControllerBase
 {
 
@@ -16,10 +17,17 @@ public class PortfoliosController : ControllerBase
         _mediator = mediator;
     }
 
-    [HttpPost]
+    [HttpPost()]
     public async Task<IActionResult> CreateRecommendedPortfolio(CreateRecommendedPortfolioCommand request)
     {
         var result = await _mediator.Send(request);
         return result.ValidateResult(this, 201);
+    }
+
+    [HttpGet("atual")]
+    public async Task<IActionResult> GetCurrentRecommendedPortfolio()
+    {
+        var result = await _mediator.Send(new GetCurrentRecommendedPortfolioQuery());
+        return result.ValidateResult(this, 200);
     }
 }
