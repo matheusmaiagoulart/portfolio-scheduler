60d3b0156c467c87c2d066f63ef9c2efeff518ea
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-05-04 13:37:10 -0300

    feat: implement DisableCustomerSubscription command and handler

---
+++ src/PortfolioScheduler.Application/Commands/DisableCustomerSubscription/DisableCustomerSubscriptionCommand.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace PortfolioScheduler.Application.Commands.DisableCustomerSubscription;
+
+public record DisableCustomerSubscriptionCommand(long customerId) : IRequest<Result<DisableCustomerSubscriptionResponse>>;
---
+++ src/PortfolioScheduler.Application/Commands/DisableCustomerSubscription/DisableCustomerSubscriptionHandler.cs
@@ -0,0 +1,39 @@
+﻿using FluentResults;
+using MediatR;
+using PortfolioScheduler.Domain.DomainErrors;
+using PortfolioScheduler.Domain.Repositories;
+
+namespace PortfolioScheduler.Application.Commands.DisableCustomerSubscription;
+
+public class DisableCustomerSubscriptionHandler : IRequestHandler<DisableCustomerSubscriptionCommand, Result<DisableCustomerSubscriptionResponse>>
+{
+    private readonly ICustomerRepository _customerRepository;
+
+    public DisableCustomerSubscriptionHandler(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public async Task<Result<DisableCustomerSubscriptionResponse>> Handle(DisableCustomerSubscriptionCommand request, CancellationToken ct)
+    {
+        var customer = await _customerRepository.GetByIdAsync(request.customerId, ct);
+        if (customer == null)
+            return Result.Fail(CustomerErrors.CustomerNotFound(request.customerId));
+
+        var resultDisable = customer.Disable();
+        if (resultDisable.IsFailed)
+            return Result.Fail(resultDisable.Errors);
+
+        var resultUpdate = await _customerRepository.UpdateAsync(customer, ct);
+        if (resultUpdate.IsFailed)
+            return Result.Fail(resultUpdate.Errors);
+
+        return Result.Ok(new DisableCustomerSubscriptionResponse
+        (
+            customerId: customer.Id,
+            name: customer.Name,
+            active: customer.Active,
+            exitDate: DateTime.UtcNow
+        ));
+    }
+}
