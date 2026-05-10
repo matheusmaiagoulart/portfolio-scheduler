3cfff6a83e8199a143cfe1ce74379c786a774235
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-05-10 17:59:37 -0300

    feat: add ITransactionRequest marker interface for transaction handling

---
+++ src/PortfolioScheduler.Application/Behaviors/ITransactionRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PortfolioScheduler.Application.Behaviors;
+
+/// <summary>
+/// Marker interface to indicate that a request should be handled within a transaction. 
+/// For Queries requests, the transaction will not be initiated and the TransactionBehavior will simply pass the request to the next handler in the pipeline.
+/// </summary>
+public interface ITransactionRequest
+{ 
+}
