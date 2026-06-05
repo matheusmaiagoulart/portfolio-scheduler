9c3e2e2bcd8e114e16e3b091f2bdc1da32a28853
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-06-05 18:40:23 -0300

    feat: add IPortfolioRebalancer interface for portfolio rebalancing operations

---
+++ src/PortfolioScheduler.Domain/Services/Interfaces/IPortfolioRebalancer.cs
@@ -0,0 +1,26 @@
+﻿using PortfolioScheduler.Domain.Entities;
+using PortfolioScheduler.Domain.Services.DTOs;
+
+namespace PortfolioScheduler.Domain.Services.Interfaces;
+
+public interface IPortfolioRebalancer
+{
+    RebalanceResultDTO SellRemovedAssets(
+        Dictionary<string, Custody> custodies,
+        BrokerageAccount masterAccount,
+        IEnumerable<string> removedTickers,
+        Dictionary<string, decimal> pricesByTicker,
+        long customerId);
+
+    RebalanceResultDTO SellExcessFromAlteredAssets(
+        Dictionary<string, Custody> custodies,
+        BrokerageAccount masterAccount,
+        IEnumerable<(string Ticker, decimal NewPercentage)> alteredAssets,
+        Dictionary<string, decimal> pricesByTicker,
+        long customerId);
+
+    void ReplaceCustodyTickers(
+        Dictionary<string, Custody> custodies,
+        IEnumerable<string> removedTickers,
+        IEnumerable<string> addedTickers);
+}
